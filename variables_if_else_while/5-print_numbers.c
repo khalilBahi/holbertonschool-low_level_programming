a202d444f663a17331bcafe5ca9b7b21520f0933
khalilBahi <7753@holbertonstudents.com>
2023-10-17 14:07:51 +0200

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; ch <= '10'; a++)
+	{
+		printf("%d\n", a);
+	}
+	return (0);
+}
