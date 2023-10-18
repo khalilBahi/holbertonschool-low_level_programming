1227d6c966435254fe55976f408760e3d44ab181
khalilBahi <7753@holbertonstudents.com>
2023-10-18 15:28:09 +0200

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower -   prints the alphabet, in lowercase
+ * Return: Always (Success)
+ */
+int _islower(int c)
+{
+	if (c >= 97)
+	{
+		return (1)
+	}
+	else
+
+		return (0)
+}
