b3b8953cc2060177f02a0eebafc4a34c6dc05d57
khalilBahi <7753@holbertonstudents.com>
2023-10-19 11:05:32 +0200

    There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,10 +1,9 @@
-#include "stdio"
-
+#include "main.h"
 /**
- * jack_bauer - Prints every minute of the day of
- *              Jack Bauer, starting from 00:00 to 23:59.
+ * jack_bauer - prints every minute
+ * Return: Always (Success)
  */
-int print_last_digit(int)
+void jack_bauer(void)
 {
 	int hour, minute;
 
