2378f981e4b1059a01f6dac90b5a0a0607e86a15
khalilBahi <7753@holbertonstudents.com>
2023-10-19 20:25:45 +0200

    The numbers speak for themselves

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers -    function that checks for a digit (0 through 9).
+ * Return: Always (Success)
+ */
+void print_numbers(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+	return (0);
+}
