b4dd0c5a6b861a8767ee79142716233d969f1545
khalilBahi <7753@holbertonstudents.com>
2023-10-19 19:58:21 +0200

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 -   prints the alphabet, in lowercase
+ * Return: Always (Success)
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+
+		return (0);
+}
