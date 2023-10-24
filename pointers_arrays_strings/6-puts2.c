cfbb643f654d79aef01f431f82f7694ba04d5f10
khalilBahi <7753@holbertonstudents.com>
2023-10-24 14:56:18 +0200

    Half the lies they tell about me aren't true

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts2 - function that reverses a string.
+ * Return: Always  (Success)
+ * @str : pointeur
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		printf("%c", str[i]);
+		i = i + 1;
+	}
+	printf("\n");
+}
