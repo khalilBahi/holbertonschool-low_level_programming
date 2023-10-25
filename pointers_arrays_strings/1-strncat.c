ca97b71fada7f88097bebbe609b885ace188a165
khalilBahi <7753@holbertonstudents.com>
2023-10-25 16:16:52 +0200

    strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcat - function that copies the string pointed
+ * @dest : pointeur
+ * @src : pointeur
+ * @n : nbr of byte
+ * Return: the pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i])
+		i++;
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+		dest[i + j] = '\0';
+	}
+	return (dest);
+}
