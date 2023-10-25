93d03dde83ad7fd9bae47c8a63ba26f433ff84fb
khalilBahi <7753@holbertonstudents.com>
2023-10-25 13:10:55 +0200

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _atoi - function that copies the string pointed
+ * @s : pointeur
+ * Return: num*sign
+ */
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int sign = 1;
+
+	while (*s)
+	{
+		if (*s == '-')
+		{
+			sign = sign - 1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			num = (num * 10) + (*s - '0');
+		}
+		else if (num > 0)
+		{
+			break;
+		}
+		s++;
+	}
+	return (num * sign);
+}
