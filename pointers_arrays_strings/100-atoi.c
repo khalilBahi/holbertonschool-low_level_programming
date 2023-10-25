40feb2eaf7d0cf1e4c34c8c937f2f02b1c8d1731
khalilBahi <7753@holbertonstudents.com>
2023-10-25 13:39:35 +0200

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,9 +11,10 @@ int _atoi(char *s)
 	unsigned int num = 0;
 	int sign = 1;
 
-	do {
+	do
+	{
 		if (*s == '-')
-			sign = sign - 1;
+			sign *= 1;
 		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
 		else if (num > 0)
