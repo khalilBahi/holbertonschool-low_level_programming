5cb980ec84547026b18e1a4ae943d17298056f6b
khalilBahi <7753@holbertonstudents.com>
2023-10-26 18:51:06 +0200

    memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _memset - function that encodes a string into 1337
+ * _memset - function that copies memory area
  * Return: s
  * @s : pointeur
  * @b : pointeur
