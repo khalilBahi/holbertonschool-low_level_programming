744463685f995262b5e8f53f3802b4e8d6589695
khalilBahi <7753@holbertonstudents.com>
2023-10-26 17:09:44 +0200

    Expect the best. Prepare for the worst. Capitalize on what comes

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -18,13 +18,27 @@ char *cap_string(char *str)
 			if (str[i] >= 'a' && str[i] <= 'z')
 				str[i] = (int)str[i] - 32;
 		}
-		else if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '.' || str[i - 1] == ',')
+		switch (str[i])
 		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] = (int)str[i] - 32;
+		case ' ':
+		case '\t':
+		case '\n':
+		case ',':
+		case ';':
+		case '.':
+		case '!':
+		case '?':
+		case '"':
+		case '(':
+		case ')':
+		case '{':
+		case '}':
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
 		}
-		else if (str[i] >= '0' && str[i] <= '9')
-			return (p);
+
 	}
 	return (p);
 }
