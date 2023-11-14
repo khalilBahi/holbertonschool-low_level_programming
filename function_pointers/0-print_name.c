dd9be493738e17631fa8cf2b5b3dc2ec43c6ab31
khalilBahi <7753@holbertonstudents.com>
2023-11-14 10:06:47 +0100

    What's my name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ *print_name - function that prints a name
+ *@name:bob
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return;
+	f(name);
+}
