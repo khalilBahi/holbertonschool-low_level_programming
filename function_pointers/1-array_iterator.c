ea0fa5c67fe5a8d49bddc85133327c7709c5ca5b
khalilBahi <7753@holbertonstudents.com>
2023-11-15 10:44:11 +0100

    If you spend too much time thinking about a thing, you'll never get it done

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -4,6 +4,7 @@
  * @array: array
  * @size: size of the array
  * @action: a pointer to the function you need to use
+ * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
