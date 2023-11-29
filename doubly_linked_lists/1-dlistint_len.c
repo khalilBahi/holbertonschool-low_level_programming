e29b4f66045493cba08a726c37dfd45f15e9f851
khalilBahi <7753@holbertonstudents.com>
2023-11-29 18:59:55 +0100

    List length

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_dlistint -  function that prints all the elements of a dlistint_t list
+ * @h:pointer to first node
+ * Return: i element count
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
+""
