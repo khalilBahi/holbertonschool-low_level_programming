72b247862b826155242d8419c881264499668d9b
khalilBahi <7753@holbertonstudents.com>
2023-11-29 16:18:06 +0100

    Add node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * add_node - function that prints all the elements of a list_t list
+ * @head:pointer to first node
+ * @str:pointeur
+ * Return: i element count
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *n = malloc(sizeof(list_t));
+
+	if (n == NULL)
+	{
+		return NULL;
+	}
+
+	n->str = strdup(str);
+	n->len = strlen(n->str);
+
+	n->next = *head;
+	*head = n;
+
+	return (n);
+}
