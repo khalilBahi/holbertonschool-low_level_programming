04e6421505b5e956c6b936718328c58a7f9802f1
khalilBahi <7753@holbertonstudents.com>
2023-11-29 18:19:17 +0100

    Add node at the end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -7,35 +7,35 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *n = malloc(sizeof(list_t));
-    list_t *l = *head;
+	list_t *n = malloc(sizeof(list_t));
+	list_t *l = *head;
 
-    if (n == NULL)
-    {
-        free(n);
-        return (NULL);
-    }
-    n->str = strdup(str);
-    if (n->str == NULL)
-    {
-        free(n);
-        return (NULL);
-    }
+	if (n == NULL)
+	{
+		free(n);
+		return (NULL);
+	}
+	n->str = strdup(str);
+	if (n->str == NULL)
+	{
+		free(n);
+		return (NULL);
+	}
 
-    n->len = strlen(n->str);
+	n->len = strlen(n->str);
 
-    n->next = NULL;
-    if (*head == NULL)
-    {
-        *head = n;
-    }
-   else
-    {
-        while (l->next!=NULL)
-        {
-            l = l->next;
-        }
-        l->next = n;
-    }
-    return (n);
+	n->next = NULL;
+	if (*head == NULL)
+	{
+		*head = n;
+	}
+	else
+	{
+		while (l->next != NULL)
+		{
+			l = l->next;
+		}
+		l->next = n;
+	}
+	return (n);
 }
