338240e0185495a878c7ab7cd33ca3f22493c1d3
khalilBahi <7753@holbertonstudents.com>
2023-11-30 14:32:44 +0100

    add node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,28 +1,27 @@
 #include "lists.h"
 /**
- * *add_node - function that prints all the elements of a list_t list
- * @head:pointer to first node
- * @n:pointeur
- * Return: nn
- */
+ * add_dnodeint - function that adds a new node
+ * at the beginning of a dlistint_t list
+ * @n: number
+ * @head: head address
+ * Return: new node address
+*/
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *nn = malloc(sizeof(dlistint_t));
 
-	if (nn != NULL)
-	{
+	if (nn == NULL)
 		return (NULL);
-	}
 
 	nn->n = n;
-	nn->prev = NULL;
-	nn->next = *head;
-
-	if (*head != NULL)
+	if (*head == NULL)
+		nn->next = NULL;
+	else
 	{
+		nn->next = *head;
 		(*head)->prev = nn;
 	}
-
 	*head = nn;
 	return (nn);
 }
+ 
