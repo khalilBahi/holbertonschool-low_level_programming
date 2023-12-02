1f235501fd6d65cf350a586e75816ad2ba07a35b
khalilBahi <7753@holbertonstudents.com>
2023-12-02 17:21:44 +0100

    Sum list

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * sum_dlistint - function that returns the sum of all the data (n) of a dlistint_t linked list
+ * sum_dlistint - function that returns the sum of the linked list
  * @head: list name
  * Return: node number (sum)
  */
