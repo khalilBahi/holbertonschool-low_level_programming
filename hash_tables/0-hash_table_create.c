abd6022655e54792a3260b5feb571a213136f201
khalilBahi <7753@holbertonstudents.com>
2023-12-09 22:10:01 +0100

    creat hash_table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -6,22 +6,22 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *t;
+	hash_table_t *t;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    t = (hash_table_t *)malloc(sizeof(hash_table_t));
-    if (t == NULL)
-    {
-        return (NULL);
-    }
-    t->size = size;
-    t->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
-    if (t->array == NULL)
-    {
-        free(t);
-        return (NULL);
-    }
-    return (t);
+	t = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (t == NULL)
+	{
+		return (NULL);
+	}
+	t->size = size;
+	t->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+	if (t->array == NULL)
+	{
+		free(t);
+		return (NULL);
+	}
+	return (t);
 }
