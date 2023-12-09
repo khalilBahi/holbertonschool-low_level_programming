6b97690a13296d7b89a9a57fec9466a7558e3c51
khalilBahi <7753@holbertonstudents.com>
2023-12-09 22:00:32 +0100

    header file

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - create hash table
+ * @size: size of the array
+ * Return: (t)
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *t;
+
+    if (size == 0)
+        return (NULL);
+
+    t = (hash_table_t *)malloc(sizeof(hash_table_t));
+    if (t == NULL)
+    {
+        return (NULL);
+    }
+    t->size = size;
+    t->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+    if (t->array == NULL)
+    {
+        free(t);
+        return (NULL);
+    }
+    return (t);
+}
